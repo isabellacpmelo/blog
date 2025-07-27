90a659caf7a6d32c7dffecc8bfa4ae623c953890
isabellacpmelo <isacpmelo@gmail.com>
2025-07-27 20:23:42 -0300

    feat: Creating a PostModel to model the post

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,14 @@
+/** @format */
+
+export type PostModel = {
+  id: string
+  title: string
+  slug: string
+  excerpt: string
+  content: string
+  coverImageUrl: string
+  published: boolean
+  createdAt: string
+  updatedAt: string
+  author: string
+}
