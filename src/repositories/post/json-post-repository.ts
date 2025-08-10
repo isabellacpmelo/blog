0a482a80a7a22ec48451e408436f972fa21ff0e5
isabellacpmelo <isacpmelo@gmail.com>
2025-08-10 20:41:28 -0300

    feat: Repository Design Pattern for Posts - Part 2

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -2,15 +2,37 @@
 
 import { PostModel } from '@/models/post/post-model'
 import { PostRepository } from './post-repository'
+import { resolve } from 'path'
+import { readFile } from 'fs/promises'
 
 const ROOT_DIR = process.cwd()
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  'src',
+  'db',
+  'seed',
+  'posts.json'
+)
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8')
+    const parsedJson = JSON.parse(jsonContent)
 
-  findAll(): Promise<PostModel[]> {}
+    const { posts } = parsedJson
+    return posts
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk()
+    return posts
+  }
 }
 
 export const postRepository = new JsonPostRepository()
-
-console.log(ROOT_DIR)
+;(async () => {
+  const posts = await postRepository.findAll()
+  posts.forEach((post) => {
+    console.log(post.author)
+  })
+})()
