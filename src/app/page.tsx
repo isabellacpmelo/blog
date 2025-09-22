fc53c066e02eb2a80f961b0196962f7d11ab22b9
isabellacpmelo <isacpmelo@gmail.com>
2025-09-22 19:35:47 -0300

    feat: Completing FeaturedPost CSS with Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,18 +16,27 @@ export default async function HomePage() {
       <section className='grid grid-cols-1 sm:grid-cols-2 gap-8 mb-16 group'>
         <Link href='#' className='w-full h-full overflow-hidden rounded-xl'>
           <Image
-            className='group-hover:scale-105'
+            className='w-full h-full object-cover object-center group-hover:scale-105'
             src='/images/bryen_0.png'
             width={1200}
             height={720}
             alt='Titulo do post'
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
-          animi vitae? Incidunt eos error beatae atque nesciunt deserunt cumque,
-          quo nostrum provident dicta quas nemo animi, vitae tempore excepturi
-          deleniti!
+        <div className='flex flex-col gap-3 sm:justify-center'>
+          <time className='text-slate-600 text-sm/tight' dateTime='2025-04-20'>
+            20/04/2025 10:00
+          </time>
+          <h1 className='text-2xl/tight font-extrabold sm:text-4xl'>
+            <Link href='#'>Lorem ipsum dolor sit amet consectetur</Link>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
+            animi vitae? Incidunt eos error beatae atque nesciunt deserunt
+            cumque, quo nostrum provident dicta quas nemo animi, vitae tempore
+            excepturi deleniti!
+          </p>
         </div>
       </section>
 
