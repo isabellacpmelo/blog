4430c8f61229dcd617421af8084260958687f6c0
isabellacpmelo <isacpmelo@gmail.com>
2025-09-22 13:02:22 -0300

    feat: FeaturedPost Component + Tailwind CSS Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,37 @@ import { Container } from '@/components/Container'
 import { Header } from '@/components/Header'
 import { PostsList } from '@/components/PostsList'
 import { SpinLoader } from '@/components/SpinLoader'
+import Link from 'next/link'
+import Image from 'next/image'
 import { Suspense } from 'react'
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 sm:grid-cols-2 gap-8 mb-16 group'>
+        <Link href='#' className='w-full h-full overflow-hidden rounded-xl'>
+          <Image
+            className='group-hover:scale-105'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Titulo do post'
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat,
+          animi vitae? Incidunt eos error beatae atque nesciunt deserunt cumque,
+          quo nostrum provident dicta quas nemo animi, vitae tempore excepturi
+          deleniti!
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
+
       <footer>
         <p className='text-6xl font-bold text-center py-8'> Aqui é o FOOTER</p>
       </footer>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 /** @format */
 
+import Link from 'next/link'
+
 export function Header() {
   return (
     <header>
       <h1 className='text-4xl/normal sm:text-5xl/normal md:text-6xl lg:text-7xl/normal font-extrabold py-6 sm:py-8 md:py-10 lg:py-12'>
-        <a href='#'>The Blog</a>
+        <Link href='#'>The Blog</Link>
       </h1>
     </header>
   )
