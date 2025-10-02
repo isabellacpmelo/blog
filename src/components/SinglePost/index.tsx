f12930e7f714d0e72f54c1cbee3d741493e8df8d
isabellacpmelo <isacpmelo@gmail.com>
2025-10-02 19:38:21 -0300

    feat: Styling SinglePost Content with Tailwind CSS

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,5 +1,8 @@
 import { findPostBySlugCached } from '@/lib/post/queries'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
+import { PostHead } from '../PostHeading'
+import { PostDate } from '../PostDate'
 
 type SinglePostProps = {
   slug: string
@@ -13,8 +16,25 @@ export async function SinglePost({ slug }: SinglePostProps) {
   if (!post) notFound()
 
   return (
-    <div>
-      <p>{post.title}</p>
-    </div>
+    <article className='mb-16'>
+      <header className='group flex flex-col gap-4 mb-4'>
+        <Image
+          src={post.coverImageUrl}
+          width={600}
+          height={360}
+          alt={post.title}
+          className='rounded-xl'
+        />
+
+        <PostHead href={`/post/${post.slug}`}>{post.title}</PostHead>
+        <p>
+          {post.author} | <PostDate dateTime={post.createdAt} />
+        </p>
+      </header>
+
+      <p className='mb-4 italic text-slate-600'>{post.excerpt}</p>
+
+      <div>{post.content}</div>
+    </article>
   )
 }
