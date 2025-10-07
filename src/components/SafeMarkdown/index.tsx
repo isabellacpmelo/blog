63a17def7fee1d1386f3e11a30cf66b525549a69
isabellacpmelo <isacpmelo@gmail.com>
2025-10-07 19:39:44 -0300

    feat: Replacing and Modifying HTML Elements in React Markdown

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -11,7 +11,18 @@ export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
     <div className='prose prose-slate w-full max-w-none overflow-hidden prose-a:transition prose-a:no-underline prose-a:hover:underline prose-a:text-blue-500 prose-a:hover:text-blue-700 prose-img:mx-auto md:prose-lg'>
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
-        remarkPlugins={[remarkGfm]}>
+        remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return ''
+
+            return (
+              <div className='overflow-x-auto'>
+                <table className='w-full min-w-[600px]' {...props} />
+              </div>
+            )
+          },
+        }}>
         {markdown}
       </ReactMarkdown>
     </div>
