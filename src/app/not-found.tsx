58239e49880f800ad19c3bcc05c04d0deca651e8
isabellacpmelo <isacpmelo@gmail.com>
2025-10-07 19:48:47 -0300

    feat: Component for error messages to the client (ErrorMessage)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,14 @@
+import { ErrorMessage } from '@/components/ErrorMessage'
+
 export default function NotFoundPage() {
   return (
     <>
-      <title>404 - Página não encontrada</title>
-      <div className='min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center'>
-        <div>
-          <h1 className='text-7xl/tight mb-4 font-extrabold'>404</h1>
-          <p>
-            Erro 404 - A página que você está tentando acessar não existe neste
-            site
-          </p>
-        </div>
-      </div>
+      <ErrorMessage
+        pageTitle='404 - Página não encontrada'
+        contentTitle='404 😥'
+        content='Erro 404 - A página que você está tentando acessar não existe neste
+            site'
+      />
     </>
   )
 }
