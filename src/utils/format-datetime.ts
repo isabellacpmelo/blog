29754e66557254718afd3e47387e52e301e1ca14
isamelo <isacpmelo@gmail.com>
2025-11-13 15:40:32 -0300

    feat: Studying the Next.js cache

--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -1,20 +1,26 @@
 import {
   format,
   formatDistanceToNow as dateFnsFormatDistanceToNow,
-} from 'date-fns'
-import { ptBR } from 'date-fns/locale'
+} from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export function formatDateTime(rawDate: string): string {
-  const date = new Date(rawDate)
+  const date = new Date(rawDate);
 
-  return format(date, "dd/MM/yyyy 'às' HH'h'mm", { locale: ptBR })
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", { locale: ptBR });
 }
 
 export function formatDistanceToNow(rawDate: string): string {
-  const date = new Date(rawDate)
+  const date = new Date(rawDate);
 
   return dateFnsFormatDistanceToNow(date, {
     locale: ptBR,
     addSuffix: true,
-  })
+  });
+}
+
+export function formatHour(timestampMs: number): string {
+  const date = new Date(timestampMs);
+
+  return format(date, "HH:mm:ss", { locale: ptBR });
 }
